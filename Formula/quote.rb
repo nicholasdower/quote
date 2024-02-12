82571c3cf75630bc27b170e71f4627e8a8668afc
Nick Dower <nicholasdower@gmail.com>
2024-02-12 17:10:22 +0100

    v1.0.0 Release
    - Quote lines.

--- Formula/quote.rb
+++ Formula/quote.rb
@@ -2,7 +2,7 @@ class Quote < Formula
   desc "Quote lines"
   homepage "https://github.com/nicholasdower/quote"
   url "https://github.com/nicholasdower/quote/releases/download/v1.0.0/release.tar.gz"
-  sha256 "TBD"
+  sha256 "57d5ea39c8138dbe2d12c7fdf3947d680750b180aa6d677bc925c5c7ede95aba"
   license "MIT"
 
   def install
